5d34c8599df9dcb2e7d6a06fbf7d4bfd8f9ef081
Nahid Hossain <nahid4510@gmail.com>
2026-02-20 23:30:27 +0600

    server setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-const { Server } = require('socket.io')
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+server.listen(3001, () => {
+    console.log('Server listening on port 3001');
+    
+})
